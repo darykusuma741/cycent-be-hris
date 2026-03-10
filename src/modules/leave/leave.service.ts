02c5e7247582b42f8da96c928d06830af2cbdedb
Dary Kusuma Hardian <darykusuma741@gmail.com>
2026-03-10 11:31:37 +0700

    leave fix

--- src/modules/leave/leave.service.ts
+++ src/modules/leave/leave.service.ts
@@ -3,6 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { ApproveLeaveDto, RejectLeaveDto, RequestMyLeaveDto } from './leave.model';
 import { UserPayload } from '@common/guard/user.payload';
 import { log } from 'node:console';
+import { stat } from 'node:fs';
 
 type LeaveStatus = 'Approved' | 'Rejected';
 
@@ -19,7 +20,35 @@ export class LeaveService {
 
     if (!employee) throw new NotFoundException('Employee not found');
 
-    const totalLeaveDays = (new Date(request.endDate).getTime() - new Date(request.startDate).getTime()) / (1000 * 3600 * 24) + 1;
+    const leavePolicy = await this.prisma.leavePolicy.findUnique({
+      where: {
+        leaveType: request.type,
+      },
+    });
+
+    if (!leavePolicy) throw new NotFoundException('Leave policy not found for the specified type');
+
+    const startDate = new Date(request.startDate);
+    const endDate = new Date(request.endDate);
+    const totalLeaveDays = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24) + 1;
+    const summedLeaveDays = await this.prisma.leave.aggregate({
+      where: {
+        employeeId: employee.id,
+        leaveType: request.type,
+        status: 'Approved',
+      },
+      _sum: {
+        totalDays: true,
+      },
+    });
+
+    const totalUsedLeaveDays = summedLeaveDays._sum.totalDays || 0;
+    const remainingLeaveDays = leavePolicy.annualQuota - totalUsedLeaveDays;
+
+    if (totalLeaveDays > remainingLeaveDays) {
+      throw new NotFoundException(`Insufficient leave balance. You have ${remainingLeaveDays} ${request.type} leave days remaining.`);
+    }
+
     log('Start Date:', request.startDate);
     log('End Date:', request.endDate);
     log('Start Date (ms):', new Date(request.startDate).getTime());
@@ -41,6 +70,44 @@ export class LeaveService {
   }
 
   async updateLeaveStatus(requestUser: Request & { user?: UserPayload }, leaveId: number, status: LeaveStatus) {
+    if (status === 'Approved') {
+      const leave = await this.prisma.leave.findUnique({
+        where: { id: leaveId },
+      });
+
+      if (!leave) throw new NotFoundException('Leave request not found');
+
+      const leavePolicy = await this.prisma.leavePolicy.findUnique({
+        where: {
+          leaveType: leave.leaveType,
+        },
+      });
+
+      if (!leavePolicy) throw new NotFoundException('Leave policy not found for the specified type');
+
+      const startDate = new Date(leave.startDate);
+      const endDate = new Date(leave.endDate);
+      const totalLeaveDays = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24) + 1;
+
+      const summedLeaveDays = await this.prisma.leave.aggregate({
+        where: {
+          employeeId: leave.employeeId,
+          leaveType: leave.leaveType,
+          status: 'Approved',
+        },
+        _sum: {
+          totalDays: true,
+        },
+      });
+
+      const totalUsedLeaveDays = summedLeaveDays._sum.totalDays || 0;
+      const remainingLeaveDays = leavePolicy.annualQuota - totalUsedLeaveDays;
+
+      if (totalLeaveDays > remainingLeaveDays) {
+        throw new NotFoundException(`Insufficient leave balance. The employee has ${remainingLeaveDays} ${leave.leaveType} leave days remaining.`);
+      }
+    }
+
     const leave = await this.prisma.leave.findUnique({
       where: { id: leaveId },
     });
